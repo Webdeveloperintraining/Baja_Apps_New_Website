e2e65b4bf9448ca9e4db0154b3466062edb5a622
Webdeveloperintraining <sil22006@byui.edu>
2023-01-23 11:24:09 -0800

    general styling and animations added

--- js/dropdown.js
+++ js/dropdown.js
@@ -5,21 +5,42 @@ function dropContent() {
     document.querySelector(".drop-trigger").classList.toggle("show");
 }
 
-const arrows= document.querySelector(".drop-trigger");
-arrows.onclick=dropContent;
+const arrow= document.querySelector(".drop-trigger");
+arrow.onclick=dropContent;
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick= function(event) {
-    if (!event.target.matches ('.drop-trigger')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var arrows = document.querySelector(".drop-trigger")
-       var i;
-       for (i = 0; i <dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')){
-            openDropdown.classList.remove('show');
-            arrows.classList.remove('show')
-        }
-       } 
-    }
+// window.onclick= function(event) {
+//     if (!event.target.matches ('.drop-trigger')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var arrows = document.querySelector(".drop-trigger")
+//        var i;
+//        for (i = 0; i <dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')){
+//             openDropdown.classList.remove('show');
+//             arrows.classList.remove('show')
+//         }
+//        } 
+//     }
+// }
+
+
+function dropContent2(){
+    document.getElementById("support_dropdown").classList.toggle("show");
+    document.querySelector(".drop-trigger2").classList.toggle("show");
 }
+
+// window.onclick= function(event) {
+//     if (!event.target.matches ('.drop-trigger2')) {
+//         var dropdowns2 = document.getElementsByClassName("dropdown-content2");
+//         var arrow = document.querySelector(".drop-trigger2")
+//        var i;
+//        for (i = 0; i < dropdowns2.length; i++) {
+//         var openDropdown = dropdowns2[i];
+//         if (openDropdown.classList.contains('show')){
+//             openDropdown.classList.remove('show');
+//             arrow.classList.remove('show')
+//         }
+//        } 
+//     }
+// }
---
+++ js/modal.js
@@ -0,0 +1,26 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+var img2 = document.getElementById("myImg");
+var modalImg2 = document.getElementById("img01");
+var captionText2 = document.getElementById("caption");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
